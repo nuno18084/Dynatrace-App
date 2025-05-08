c216c674c35735abce59bd8c1f9b410eedf9589c
Nuno Colaço <nunocolaco@Nunos-MBP.Home>
2025-05-08 21:09:16 +0100

    create a component for the table

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import { downloadCSV } from "./utils/downloadCSV";
 import axios from "axios";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import DataTable from "./components/DataTable/DataTable";
 
 function App() {
   const { data, setData } = useData();
@@ -61,77 +62,7 @@ function App() {
       />
       <h1 className="title">Dynatrace Data</h1>
 
-      <div className="table-container">
-        <table>
-          <thead>
-            <tr>
-              {selectedColumns.map((col) => (
-                <th key={col}>{col}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item, index) => (
-              <tr key={index}>
-                {selectedColumns.map((col) => {
-                  let value = item[col];
-
-                  if (
-                    col === "firstSeenTimestamp" ||
-                    col === "lastSeenTimestamp"
-                  ) {
-                    value = new Date(value).toLocaleString();
-                  }
-
-                  if (col === "fromRelationships" && value) {
-                    return (
-                      <td key={col}>
-                        {Object.keys(value).map((k) => (
-                          <div key={k}>
-                            {k}: {JSON.stringify(value[k])}
-                          </div>
-                        ))}
-                      </td>
-                    );
-                  }
-
-                  if (col === "properties" && value) {
-                    return (
-                      <td key={col}>
-                        {Object.keys(value).map((k) => (
-                          <div key={k}>
-                            {k}: {JSON.stringify(value[k])}
-                          </div>
-                        ))}
-                      </td>
-                    );
-                  }
-
-                  if (col === "tags" && Array.isArray(value)) {
-                    return (
-                      <td key={col}>
-                        {value.map((tag, i) => (
-                          <div key={i}>
-                            {tag.key}: {tag.value}
-                          </div>
-                        ))}
-                      </td>
-                    );
-                  }
-
-                  return (
-                    <td key={col}>
-                      {typeof value === "object"
-                        ? JSON.stringify(value)
-                        : value}
-                    </td>
-                  );
-                })}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <DataTable data={data} selectedColumns={selectedColumns} />
 
       <button className="download-btn" onClick={handleDownloadCSV}>
         Download CSV
---
+++ client/src/components/DataTable/DataTable.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import "./DataTable.css";
+
+const DataTable = ({ data, selectedColumns }) => {
+  return (
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr>
+            {selectedColumns.map((col) => (
+              <th key={col}>{col}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              {selectedColumns.map((col) => {
+                let value = item[col];
+
+                if (
+                  col === "firstSeenTimestamp" ||
+                  col === "lastSeenTimestamp"
+                ) {
+                  value = new Date(value).toLocaleString();
+                }
+
+                if (col === "fromRelationships" && value) {
+                  return (
+                    <td key={col}>
+                      {Object.keys(value).map((k) => (
+                        <div key={k}>
+                          {k}: {JSON.stringify(value[k])}
+                        </div>
+                      ))}
+                    </td>
+                  );
+                }
+
+                if (col === "properties" && value) {
+                  return (
+                    <td key={col}>
+                      {Object.keys(value).map((k) => (
+                        <div key={k}>
+                          {k}: {JSON.stringify(value[k])}
+                        </div>
+                      ))}
+                    </td>
+                  );
+                }
+
+                if (col === "tags" && Array.isArray(value)) {
+                  return (
+                    <td key={col}>
+                      {value.map((tag, i) => (
+                        <div key={i}>
+                          {tag.key}: {tag.value}
+                        </div>
+                      ))}
+                    </td>
+                  );
+                }
+
+                return (
+                  <td key={col}>
+                    {typeof value === "object" ? JSON.stringify(value) : value}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DataTable;
